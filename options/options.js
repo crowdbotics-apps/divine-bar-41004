beadfb0cc501312e9764dc05a09d4987750f9a61
Crowdbotics <team@crowdbotics.com>
2023-05-25 21:56:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-bar-41004",
+  url: "https://divine-bar-41004.botics.co",
+  api: "https://divine-bar-41004.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
